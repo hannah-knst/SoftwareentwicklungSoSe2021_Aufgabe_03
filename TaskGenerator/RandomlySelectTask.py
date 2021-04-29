42277a4beca518df4c9be060e693c8789648eaf1
Sebastian Zug <Sebastian.Zug@informatik.tu-freiberg.de>
2021-04-29 10:51:28 +0200

    Update RandomlySelectTask.py

--- TaskGenerator/RandomlySelectTask.py
+++ TaskGenerator/RandomlySelectTask.py
@@ -1,1 +1,23 @@
-print("Hello World");
+import random 
+
+items = [
+    'Variant_0.md',
+    'Variant_1.md'
+]
+
+readmefilename = "../README.md"
+
+taskname = random.sample(items,  1)
+
+with open(readmefilename, 'r') as readmefilehandle:
+        readmecontent = readmefilehandle.read()
+
+with open(taskname, 'r') as taskfilehandle:
+        taskcontent = taskfilehandle.read()
+
+newreadmecontent = readmecontent.split("## Aufgabenvariante")[0] + taskcontent
+
+with open(readmefilename, 'w') as readmefilehandle:
+        readmefilehandle.write(newreadmecontent)
+
+print("Aus die Maus!")
