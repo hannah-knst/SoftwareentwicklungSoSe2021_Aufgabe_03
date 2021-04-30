186937c144e95ebbbf22c555e0b9a9de53214634
Sebastian Zug <Sebastian.Zug@informatik.tu-freiberg.de>
2021-04-30 14:47:04 +0000

    Reconstruct python file

--- .github/workflows/InitiallySelectTask.py
+++ .github/workflows/InitiallySelectTask.py
@@ -1,11 +1,3 @@
-import os
-
-git_message = os.environ['MESSAGE']
-git_user = os.environ['USER']
-
-print(git_message)
-print(git_user)
-
 # The script replace actual task descriptions by copying content from individual task 
 # files to a specific task sheet. 
 # 
@@ -19,35 +11,45 @@ print(git_user)
 #   +-----+                        | Part        | 
 #                                  +-------------+
 
+import os
 import random 
-import logging
 
-items = [
-    './.github/workflows/tasks/Variant_0.md',
-    './.github/workflows/tasks/Variant_1.md'
-]
-
+git_message = os.environ['MESSAGE']
+git_user = os.environ['USER']
 readmefilename = "./README.md"
 loggingfilename = "./.content/task.log"
 splitingWords="## Aufgabenvariante"
 
-taskname = random.sample(items,  1)[0]
-print(taskname)
+print(git_message)
+print(git_user)
 
-# Read from original README.md
-with open(readmefilename, 'r') as readmefilehandle:
-        readmecontent = readmefilehandle.read()
+if git_user == "github-classroom":
 
-# Read actual task description
-with open(taskname, 'r') as taskfilehandle:
-        taskcontent = taskfilehandle.read()
-
-newreadmecontent = readmecontent.split(splitingWords)[0] + taskcontent
-
-# Write adapted README.md
-with open(readmefilename, 'w') as readmefilehandle:
-        readmefilehandle.write(newreadmecontent)
-
-# Write task identifier to separate file
-with open(loggingfilename, 'w') as loggingfilehandle:
-        loggingfilehandle.write(taskname)
+  items = [
+    './.github/workflows/tasks/Variant_0.md',
+    './.github/workflows/tasks/Variant_1.md'
+  ]
+   
+  # Select task file
+  taskname = random.sample(items,  1)[0]
+  print(taskname)
+  
+  # Read from original README.md
+  with open(readmefilename, 'r') as readmefilehandle:
+          readmecontent = readmefilehandle.read()
+  
+  # Read actual task description
+  with open(taskname, 'r') as taskfilehandle:
+          taskcontent = taskfilehandle.read()
+  
+  newreadmecontent = readmecontent.split(splitingWords)[0] + taskcontent
+  
+  # Write adapted README.md
+  with open(readmefilename, 'w') as readmefilehandle:
+          readmefilehandle.write(newreadmecontent)
+  
+  # Write task identifier to separate file
+  with open(loggingfilename, 'w') as loggingfilehandle:
+          loggingfilehandle.write(taskname)
+ 
+  os.environ['TASK'] = taskname
